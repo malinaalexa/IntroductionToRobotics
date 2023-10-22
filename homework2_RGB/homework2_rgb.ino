b07061dad948983cb0dcd25f369bb66131bd8d49
Steica Malina-Aexa <104028370+malinaalexa@users.noreply.github.com>
2023-10-22 22:47:53 +0300

    homework2_rgb.ino

--- homework2_RGB/homework2_rgb.ino
+++ homework2_RGB/homework2_rgb.ino
@@ -1,3 +1,6 @@
+const int controlValue=1023;
+const int ledValue=255;
+
 const int controlRPin=A0;
 const int controlGPin=A1;
 const int controlBPin=A2;
@@ -12,6 +15,7 @@ int ledRValue=0;
 int ledGValue=0;
 int ledBValue=0;
 
+
 void setup() {
   pinMode(controlRPin, INPUT);
   pinMode(controlGPin, INPUT);
@@ -25,9 +29,9 @@ void loop() {
   controlRValue=analogRead(controlRPin);
   controlGValue=analogRead(controlGPin);
   controlBValue=analogRead(controlBPin);
-  ledRValue=map(controlRValue, 0, 1023, 0, 255);
-  ledGValue=map(controlGValue, 0, 1023, 0, 255);
-  ledBValue=map(controlBValue, 0, 1023, 0, 255);
+  ledRValue=map(controlRValue, 0, controlValue, 0, ledValue);
+  ledGValue=map(controlGValue, 0, controlValue, 0, ledValue);
+  ledBValue=map(controlBValue, 0, controlValue, 0, ledValue);
   analogWrite(ledRPin, ledRValue);
   analogWrite(ledGPin, ledGValue);
   analogWrite(ledBPin, ledBValue);
